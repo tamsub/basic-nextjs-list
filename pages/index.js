bad1e6d3f67d6a53cdce9906e04338981f844144
tamsub <soptam177@gmail.com>
2022-12-21 14:28:30 +0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
